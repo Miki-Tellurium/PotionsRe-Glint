c38d845528eb5fd35ebc8370add74e57f8d2062e
Miki-Tellurium <michelegamer14@gmail.com>
2023-03-15 17:41:14 +0100

    Added enchantment glint to potions

--- src/main/java/com/mikitellurium/potionsreglint/mixin/PotionItemMixin.java
+++ src/main/java/com/mikitellurium/potionsreglint/mixin/PotionItemMixin.java
@@ -1,16 +1,23 @@
 package com.mikitellurium.potionsreglint.mixin;
 
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 
 @Mixin(PotionItem.class)
 public class PotionItemMixin extends Item {
 
-    public PotionItemMixin(Properties p_41383_) {
-        super(p_41383_);
+    public PotionItemMixin(Properties properties) {
+        super(properties);
     }
 
-
+    // Override the isFoil method of Item class in the PotionItem class
+    @Override
+    public boolean isFoil(@NotNull ItemStack stack) {
+        return super.isFoil(stack) || PotionUtils.getPotion(stack).isFoil(stack);
+    }
 
 }
