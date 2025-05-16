25887d2c4abd9114b02a8b6520ef76023e505dc3
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-16 16:23:47 +0200

    Update to 1.20.5

---
+++ src/main/java/com/mikitellurium/potionsreglint/config/screen/ConfigScreen.java
@@ -0,0 +1,29 @@
+package com.mikitellurium.potionsreglint.config.screen;
+
+import com.mikitellurium.potionsreglint.config.Configuration;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class ConfigScreen {
+    public static Screen openScreen(Minecraft minecraft, Screen parent) {
+        ConfigBuilder builder = ConfigBuilder.create()
+                .setParentScreen(parent)
+                .setTitle(Component.translatable("config.potionsreglint.title"));
+
+        ConfigCategory general = builder.getOrCreateCategory(Component.empty());
+
+        ConfigEntryBuilder entryBuilder = builder.entryBuilder();
+
+        general.addEntry(entryBuilder.startBooleanToggle(
+                Component.translatable("config.potionsreglint.enablePotionGlint"), Configuration.ENABLE_POTION_GLINT.get())
+                .setDefaultValue(true)
+                .setSaveConsumer((newValue) -> Configuration.ENABLE_POTION_GLINT.set(newValue))
+                .build());
+
+        return builder.build();
+    }
+}
---
+++ src/main/java/com/mikitellurium/potionsreglint/mixin/PotionItemMixin.java
@@ -0,0 +1,28 @@
+package com.mikitellurium.potionsreglint.mixin;
+
+import com.mikitellurium.potionsreglint.config.Configuration;
+import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionContents;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PotionItem.class)
+public class PotionItemMixin extends Item {
+    public PotionItemMixin(Properties properties) {
+        super(properties);
+    }
+
+    // Override the isFoil method of Item class in the PotionItem class
+    @Override
+    public boolean isFoil(@NotNull ItemStack stack) {
+        if (Configuration.ENABLE_POTION_GLINT.get()) {
+            PotionContents potionContents = stack.get(DataComponents.POTION_CONTENTS);
+            return super.isFoil(stack) || (potionContents != null && potionContents.hasEffects());
+        } else {
+            return false;
+        }
+    }
+}
