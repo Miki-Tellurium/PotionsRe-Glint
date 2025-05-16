da188afc9e0b52f6777e0c9f0387923d9e38c8dd
Miki_Tellurium <michelegamer14@gmail.com>
2025-05-16 11:45:16 +0200

    Commit version 1.20.2

---
+++ src/main/java/com/mikitellurium/potionsreglint/mixin/PotionItemMixin.java
@@ -0,0 +1,26 @@
+package com.mikitellurium.potionsreglint.mixin;
+
+import com.mikitellurium.potionsreglint.config.Configuration;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PotionItem;
+import net.minecraft.world.item.alchemy.PotionUtils;
+import org.jetbrains.annotations.NotNull;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(PotionItem.class)
+public class PotionItemMixin extends Item {
+    public PotionItemMixin(Properties properties) {
+        super(properties);
+    }
+
+    // Override the isFoil method of Item class in the PotionItem class
+    @Override
+    public boolean isFoil(@NotNull ItemStack stack) {
+        if (Configuration.ENABLE_POTION_GLINT.get()) {
+            return super.isFoil(stack) || !PotionUtils.getPotion(stack).getEffects().isEmpty();
+        } else {
+            return false;
+        }
+    }
+}
